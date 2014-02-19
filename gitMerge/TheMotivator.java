f02af470d3f68e1feeb7989d4543ce3298f535b7
Shawn Johnson <shsjohns@mymail.mines.edu>
2014-02-19 15:08:23 -0700

    added a negative score option

--- gitMerge/TheMotivator.java
+++ gitMerge/TheMotivator.java
@@ -8,23 +8,19 @@ public class TheMotivator {
 			System.out.println("That's great");
 		else if (score > 60)
 			System.out.println("That's good ");
-<<<<<<< HEAD
 		else if (score > 50){
 			System.out.println("You can do better.");
-=======
-		else if ( score < 30){
+		}else if ( score < 0){
+			System.out.println("You can't have a negative score");
+		}else if(score < 30){
 			System.out.println("You can suck less than that.");
->>>>>>> newBranch
 		}else
 			System.out.println("Well, what can I say?");
 	}
 	public static void main(String[] args) {
 		TheMotivator tm = new TheMotivator();
-<<<<<<< HEAD
 		tm.feedback(60);
-=======
-		tm.feedback(20);
->>>>>>> newBranch
+		tm.feedback(-10);
 	}
 
 }

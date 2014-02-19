bacd95c466c4296321e77f51048d75fa95571e0f
Shawn Johnson <shsjohns@mymail.mines.edu>
2014-02-19 15:02:22 -0700

    Added less than 30 option

--- gitMerge/TheMotivator.java
+++ gitMerge/TheMotivator.java
@@ -1,10 +1,21 @@
 package gitMerge;
 
 public class TheMotivator {
-
+	public void feedback(int score) {
+		if (score == 100)
+			System.out.println("You're awesome");
+		else if (score > 90)
+			System.out.println("That's great");
+		else if (score > 60)
+			System.out.println("That's good ");
+		else if ( score < 30){
+			System.out.println("You can suck less than that.");
+		}else
+			System.out.println("Well, what can I say?");
+	}
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		TheMotivator tm = new TheMotivator();
+		tm.feedback(20);
 	}
 
 }

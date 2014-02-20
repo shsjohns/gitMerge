ee7e9ac699b3f3d9a473f91a8e5a902ea22700d0
Victoria Kay <vkay@mymail.mines.edu>
2014-02-20 16:19:51 -0700

    Added stuffy stuff -V

--- gitMerge/TheMotivator.java
+++ gitMerge/TheMotivator.java
@@ -8,9 +8,11 @@ public class TheMotivator {
 			System.out.println("That's great");
 		else if (score > 60)
 			System.out.println("That's good ");
-		else if (score > 50){
+		else if (score > 50)
 			System.out.println("You can do better.");
-		}else if ( score < 0){
+		else if (score > 40)
+			System.out.println("Stuffy stuff");
+		else if ( score < 0){
 			System.out.println("You can't have a negative score");
 		}else if(score < 30){
 			System.out.println("You can suck less than that.");

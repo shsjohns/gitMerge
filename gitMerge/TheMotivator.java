f9f79dbef510e64893a19e8fc605f5b75414708d
Victoria Kay <vkay@mymail.mines.edu>
2014-02-20 16:30:33 -0700

    Added over 100 option -V

--- gitMerge/TheMotivator.java
+++ gitMerge/TheMotivator.java
@@ -4,6 +4,8 @@ public class TheMotivator {
 	public void feedback(int score) {
 		if (score == 100)
 			System.out.println("You're awesome");
+		else if (score > 100)
+			System.out.println("Woah! You're a genius!");
 		else if (score > 90)
 			System.out.println("That's great");
 		else if (score > 60)
